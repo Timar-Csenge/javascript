f924a0b9ac455366191cdfb7dae1c5f9877c99d5
🐢 <timar.csenge.eva@diak.szbi-pg.hu>
2025-01-28 09:45:46 +0100

    link

---
+++ console_log_típusok.js
@@ -0,0 +1,12 @@
+console.log("ez egy normál üzenet");
+
+console.warn("ez egy figyelmeztetés");
+
+console.error("ez egy hibaüznet");
+
+console.table(
+    [
+        {név: "József", életkor: 22},
+        {név: "Anna", életkor: 22}
+    ]
+);
