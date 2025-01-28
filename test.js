351018fe8a5e9c73051bc43c0c7dd5b3ed04c3ff
🐢 <timar.csenge.eva@diak.szbi-pg.hu>
2025-01-28 09:22:24 +0100

    img del

--- test.js
+++ test.js
@@ -3,5 +3,9 @@ console.log("hello world");
 console.log("az első javascript kódom");
 
 function kepBeilleszt() {
-    document.getElementById("image").innerHTML = "<img src='H:/2024-25/web/2024-25/25_01_28/javascript/gugya.jpg'>";
+    document.getElementById("image").innerHTML = "<img src='H:/2024-25/web/2024-25/25_01_28/javascript/gugya.jpg' width = '500px'>";
+}
+
+function kepTorol() {
+    document.getElementById("del").innerHTML = image.innerHTML = "";
 }
