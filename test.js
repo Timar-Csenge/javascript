0fb33961d49bf52ff06a600fa4c940760312209b
🐢 <timar.csenge.eva@diak.szbi-pg.hu>
2025-01-28 09:00:08 +0100

    summary

--- test.js
+++ test.js
@@ -1,1 +1,7 @@
 console.log("hello world");
+
+console.log("az első javascript kódom");
+
+function kepBeilleszt() {
+    document.getElementById("image").innerHTML = "<img src='https://codingartistweb.com/wp-content/uploads/2022/07/day-night-01.png'>";
+}
